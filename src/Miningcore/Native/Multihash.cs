46bca58753fb1910788727f9a0452ef85c948b1a
underoath <smithrey1986@gmail.com>
2025-09-11 22:13:04 -0500

    Update Multihash.cs

--- src/Miningcore/Native/Multihash.cs
+++ src/Miningcore/Native/Multihash.cs
@@ -223,6 +223,9 @@ public static unsafe class Multihash
     [DllImport("libmultihash", EntryPoint = "yespowerTIDE_export", CallingConvention = CallingConvention.Cdecl)]
     public static extern void yespowerTIDE(byte* input, void* output, uint inputLength);
 
+    [DllImport("libmultihash", EntryPoint = "yespowerADVC_export", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void yespowerADVC(byte* input, void* output, uint inputLength);
+    
     [DllImport("libmultihash", EntryPoint = "flex_export", CallingConvention = CallingConvention.Cdecl)]
     public static extern void flex(byte* input, void* output);
 
