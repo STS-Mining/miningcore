16870ff79b27a582cb65f1e088f94a3335fe10fd
underoath <smithrey1986@gmail.com>
2025-09-11 22:18:06 -0500

    Update exports.cpp

--- src/Native/libmultihash/exports.cpp
+++ src/Native/libmultihash/exports.cpp
@@ -434,6 +434,11 @@ extern "C" MODULE_API void power2b_export(const char *input, char *output, uint3
     power2b_hash(input, output, input_len);
 }
 
+extern "C" MODULE_API void yespowerADVC_export(const char *input, char *output, uint32_t input_len)
+{
+    yespowerADVC_hash(input, output, input_len);
+}
+
 extern "C" MODULE_API void yespower_export(const char *input, char *output, uint32_t input_len)
 {
     yespower_hash(input, output, input_len);
